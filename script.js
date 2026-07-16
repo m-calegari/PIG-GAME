d7c2e25b076eebdd3406514048d4b455ec075e3b
m-calegari <mcalegari9@gmail.com>
2026-07-16 13:52:13 -0300

    commented on

--- script.js
+++ script.js
@@ -15,38 +15,50 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+// Declaration of variables
 let diceRandom;
 let currentScore = 0;
 let currentPlayer = 0;
 
+// Hidden the dice before game starts
 diceImg.style.display = 'none';
 
+// Find the index of the current player container
 const findCurrentPlayer = () =>
   [...players].findIndex(player => player.classList.contains('player--active'));
 
+// Function to switch players
 const switchPlayer = function () {
+  // Reset to zero the current score of the current player
   currentScoreElement[currentPlayer].textContent = 0;
+  // Put the index of the current player inside a variable
   currentPlayer = findCurrentPlayer();
+  // Remove the class 'player--active' from current player
   players[currentPlayer].classList.remove('player--active');
+  // Calc the next index to add the class 'player--active'
   currentPlayer = (currentPlayer + 1) % players.length;
   players[currentPlayer].classList.add('player--active');
-  diceRandom = 0;
   currentScore = 0;
 };
 
+// Button to reset the game
 btnNew.addEventListener('click', function () {
+  // Hidden the dice
   diceImg.style.display = 'none';
+  // Remove the class 'player--active' from the current player
   players[findCurrentPlayer()].classList.remove('player--active');
+  // Add the class 'player--active' randomly
   players[Math.trunc(Math.random() * players.length)].classList.add(
     'player--active',
   );
+  // Set 0 at all score elements
   scoreElement.forEach(sc => {
     sc.textContent = 0;
   });
+  // Set 0 at all current score elements
   currentScoreElement.forEach(csc => {
     csc.textContent = 0;
   });
-  diceRandom = 0;
   currentScore = 0;
 });
 
@@ -64,14 +76,18 @@ btnRoll.addEventListener('click', function () {
     // Passes the value from the variable to the element
     currentScoreElement[currentPlayer].textContent = currentScore;
   } else {
+    // Call function
     switchPlayer();
   }
 });
 
+// Button to switch player and increase the score
 btnHold.addEventListener('click', function () {
+  // Select the current player and increase it score
   scoreElement[currentPlayer].textContent =
     Number(scoreElement[currentPlayer].textContent) + currentScore;
+  // Set the current score of the current player to 0
   currentScoreElement[currentPlayer].textContent = 0;
-
+  // Call function
   switchPlayer();
 });
